16025cea853d137ee987e04e1e405744f7458149
Luis Miguel Cote <ing.luismiguelcote@gmail.com>
2026-02-22 18:36:27 -0500

    se agrega las credenciales nesesarias para conectar al backend

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_http_client.h"
+
+#include "secrets.h"
+static const char *TAG = "UV_DEVICE";
 void app_main(void)
 {
     while (1)
