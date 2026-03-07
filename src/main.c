842b2d3b0fee47ded3de7e921eeb591e7372e5cc
Luis Miguel Cote <ing.luismiguelcote@gmail.com>
2026-03-07 15:50:15 -0500

    ---

--- src/main.c
+++ src/main.c
@@ -10,14 +10,12 @@
 #include "nvs_flash.h"
 #include "esp_netif.h"
 #include "esp_http_client.h"
-#include "esp_crt_bundle.h"   
+#include "esp_crt_bundle.h"
 #include "secrets.h"
 #include "esp_netif_sntp.h"
 #include "driver/adc.h"
 
-
 static const char *TAG = "UV_DEVICE";
-
 
 static void initialize_sntp(void)
 {
@@ -67,20 +65,19 @@ static void wifi_init(void)
 
     ESP_LOGI(TAG, "Conectando a WiFi...");
 }
-
 static void send_uv_data(float uv_value)
 {
     char post_data[200];
-
-    sprintf(post_data,
-            "{\"api_key\":\"%s\",\"valor_uv\":%.2f}",
-            API_KEY,
-            uv_value);
+    ESP_LOGI(TAG, "URL: %s", API_URL);
+snprintf(post_data, sizeof(post_data),
+         "{\"api_key\":\"%s\",\"valor_uv\":%.2f}",
+         API_KEY,
+         uv_value);
 
     esp_http_client_config_t config = {
         .url = API_URL,
         .method = HTTP_METHOD_POST,
-        .crt_bundle_attach = esp_crt_bundle_attach,  // ← IMPORTANTE
+        .crt_bundle_attach = esp_crt_bundle_attach, // ← IMPORTANTE
     };
 
     esp_http_client_handle_t client = esp_http_client_init(&config);
@@ -105,15 +102,15 @@ static void send_uv_data(float uv_value)
 }
 static void adc_init(void)
 {
-    adc1_config_width(ADC_WIDTH_BIT_12);  // Resolución 0 - 4095
-    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11); 
+    adc1_config_width(ADC_WIDTH_BIT_12); // Resolución 0 - 4095
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11);
     // ADC1_CHANNEL_6 = GPIO34
 }
 static float read_uv_sensor(void)
 {
     int raw_sum = 0;
 
-    for(int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
     {
         raw_sum += adc1_get_raw(ADC1_CHANNEL_6);
         vTaskDelay(pdMS_TO_TICKS(10));
@@ -132,27 +129,42 @@ static float read_uv_sensor(void)
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
-   
 
     wifi_init();
 
+    wifi_ap_record_t ap_info;
+
+    while (esp_wifi_sta_get_ap_info(&ap_info) != ESP_OK)
+    {
+        ESP_LOGI(TAG, "Esperando conexión WiFi...");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
     vTaskDelay(pdMS_TO_TICKS(5000)); // Espera a que se establezca la conexión WiFi
 
-     initialize_sntp();
-     vTaskDelay(pdMS_TO_TICKS(5000));
+    initialize_sntp();
+    vTaskDelay(pdMS_TO_TICKS(5000));
 
-     adc_init(); 
+    adc_init();
 
-    while (1)
+while (1)
+{
+    wifi_ap_record_t ap_info;
+
+    if (esp_wifi_sta_get_ap_info(&ap_info) != ESP_OK)
     {
-        float uv_value = read_uv_sensor();
-
-        ESP_LOGI(TAG, "Enviando UV: %.2f", uv_value);
-
-        send_uv_data(uv_value);
-
-        vTaskDelay(pdMS_TO_TICKS(180000)); // 3 minutos, esto se cambiar a futuro por un delay muy pequeño, para utilizar
-        //contador par que sea mucho mas rapido, por ejemplo si se va el wifi, detecte de una que no hay conexion
-        //reinicie el dispositivo o vuelva a tratar de conectarse
+        ESP_LOGE(TAG, "WiFi perdido, reconectando...");
+        esp_wifi_connect();
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        continue;
     }
+
+    float uv_value = read_uv_sensor();
+
+    ESP_LOGI(TAG, "Enviando UV: %.2f", uv_value);
+
+    send_uv_data(uv_value);
+
+    vTaskDelay(pdMS_TO_TICKS(180000));
+}
 }
